2014e2123a98e94ed24477c09e7bc271eb82fc3f
unlomtrois <vadim.kuz02@gmail.com>
2021-06-18 12:16:42 +0300

    use ints in ids

--- src/db.js
+++ src/db.js
@@ -13,24 +13,24 @@ const db = new Sequelize(process.env.DATABASE_URL, {
 
 export class AuthTokenEmail extends Model { }
 AuthTokenEmail.init({
-  id: { type: DataTypes.STRING, primaryKey: true },
+  id: { type: DataTypes.INTEGER, primaryKey: true },
   email: DataTypes.STRING
 }, { sequelize: db, modelName: 'auth_token_email' });
 
 export class WaitForEmail extends Model { }
 WaitForEmail.init({
-  id: { type: DataTypes.STRING, primaryKey: true }
+  id: { type: DataTypes.INTEGER, primaryKey: true }
 }, { sequelize: db, modelName: 'wait_for_email' });
 
 
 export class WaitForPassword extends Model { }
 WaitForPassword.init({
-  id: { type: DataTypes.STRING, primaryKey: true }
+  id: { type: DataTypes.INTEGER, primaryKey: true }
 }, { sequelize: db, modelName: 'wait_for_password' });
 
 export class AccessToken extends Model { }
 AccessToken.init({
-  id: { type: DataTypes.STRING, primaryKey: true },
+  id: { type: DataTypes.INTEGER, primaryKey: true },
   value: DataTypes.STRING
 }, { sequelize: db, modelName: 'access_token' });
 
