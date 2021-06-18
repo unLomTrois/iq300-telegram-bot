984a638deb3fa5c1b4846342828829e667c53a5e
unlomtrois <vadim.kuz02@gmail.com>
2021-06-18 12:10:35 +0300

    rejectUnauthorized

--- src/db.js
+++ src/db.js
@@ -4,7 +4,10 @@ const { Sequelize, Model, DataTypes } = seq;
 const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   dialectOptions: {
-    ssl: true
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+    }
   }
 });
 
