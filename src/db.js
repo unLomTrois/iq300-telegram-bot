b9007101254d3b2ab7fa080c2670ca4536d32b32
unlomtrois <vadim.kuz02@gmail.com>
2021-06-18 12:07:59 +0300

    enable ssl for postgres sequelize

--- src/db.js
+++ src/db.js
@@ -1,7 +1,12 @@
 import seq from 'sequelize';
 const { Sequelize, Model, DataTypes } = seq;
 
-const db = new Sequelize(process.env.DATABASE_URL);
+const db = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: true
+  }
+});
 
 export class AuthTokenEmail extends Model { }
 AuthTokenEmail.init({
